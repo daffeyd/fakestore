07c63b39b7496495c3d17a44f5492a377eb4bebb
Daffeyd Wilbert <daffeydwilbert@gmail.com>
2024-03-13 12:33:49 +0700

    Use Configure Store

--- Redux/src/redux/store.jsx
+++ Redux/src/redux/store.jsx
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit'
+import { productsReducer, selectedProductsReducer } from "./reducers/productsReducer";
 
-import reducers from "./reducers/index";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const store = configureStore({
+    reducer: {
+        allProducts: productsReducer,
+        product: selectedProductsReducer,
+    },
+});
 
 export default store;
